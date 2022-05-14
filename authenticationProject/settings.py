9593e59b05f64515033bc699aac3b93fd400fd31
AngelBuitrago1 <angelmbm22@gmail.com>
2022-05-14 12:44:19 -0500

    Configuración proyecto

--- authenticationProject/settings.py
+++ authenticationProject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+# importo la libreria datetime porque la variable simple_jwt la usa
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +43,19 @@ INSTALLED_APPS = [
     'dogApp',
 ]
 
-# Se agregó 'rest_framework' y 'dogApp'
+# Se agregó 'rest_framework' y 'dogApp' arriba
+# Se crea la variable que indica los atributos de los tokens
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+    'ALGORITHM': 'HS256',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+} 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -52,6 +66,18 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# esta configuración permite a Simple JWT el acceso al sistema de autenticación de DRF, 
+# pues es necesario para la creación y verificación de los tokens
+
+REST_FRAMEWORK = {
+        'DEFAULT_PERMISSION_CLASSES': (
+                'rest_framework.permissions.AllowAny',
+        ),
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+                'rest_framework_simplejwt.authentication.JWTAuthentication',
+        )
+}
 
 ROOT_URLCONF = 'authenticationProject.urls'
 
@@ -76,11 +102,16 @@ WSGI_APPLICATION = 'authenticationProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+# La BD postgresql esta alojada en heroku 
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dd1sbhqaj0jfd8',
+        'USER': 'kcpoogojwwksjf',
+        'PASSWORD': '4d19803074f288f99527b37ec334e07854f3c95ff2cc28c63ded7cf67e3d6464',
+        'HOST': 'ec2-54-86-224-85.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
